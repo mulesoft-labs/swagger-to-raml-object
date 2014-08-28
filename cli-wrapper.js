8d234f3aabcb21dc40db925bda60bcc7075ab81c
Mark <wei.xiaoma@gmail.com>
2014-08-28 14:12:00 -0700

    adds cli-wrapper so the parser can be called from node

---
+++ cli-wrapper.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+var cliArgs = process.argv.slice(2);
+if (cliArgs.length < 2) {
+  console.error("Usage: swagger-to-raml <resourceListingFile> <1st apiDeclarationFile> <2nd apiDeclarationFile>...");
+  console.error("One resource listing file and at least one api declaration file is required.");
+  process.exit(1);
+}
+
+var fs = require('fs');
+var converter = require('./swagger-to-raml-object.js');
+
+fs.readFile(cliArgs[0], function (err, data){
+  if (err) {
+    console.error("Error opening file:", cliArgs[0]);
+    process.exit(1);
+  };
+  var listingObj = JSON.parse(data.toString());
+  console.log(converter.resourceListing(listingObj, {}));
+});
