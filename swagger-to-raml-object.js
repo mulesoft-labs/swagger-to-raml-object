24763db7fd63f82f10cd4c653c20dbe53c483e32
Mark <wei.xiaoma@gmail.com>
2014-08-31 18:05:17 -0700

    converts non-title portions of swagger info to raml documentation items

--- swagger-to-raml-object.js
+++ swagger-to-raml-object.js
@@ -2,14 +2,19 @@ var _ = require('lodash');
 
 var parseResourceListing = function(resourceListing, ramlObj) {
   resourceListing = resourceListing || {};
-  ramlObj = ramlObj || {};
+  ramlObj = ramlObj || {documentation: []};
 
   var convertInfo = function(info) {
-    if (!info.title) throw "Invalid Swagger Resource Listing: title is a required property of info";
     ramlObj.title = info.title;
-    console.log("ramlObj: ", ramlObj);
-    if (!info.description) throw "Invalid Swagger Resource Listing: description is a required property of info";
-    // RAML has no analogous top-level description
+    // RAML has no analogous top-level description, so use "documentation"
+    if (info.description) ramlObj.documentation.push({
+      title: "description",
+      content: info.description
+    });
+    if (info.termsOfServiceUrl) ramlObj.documentation.push({title: "termsOfServiceUrl", content: info.termsOfServiceUrl});
+    if (info.contact) ramlObj.documentation.push({title: "contact", content: info.contact});
+    if (info.license) ramlObj.documentation.push({title: "license", content: info.license});
+    if (info.licenseUrl) ramlObj.documentation.push({title: "licenseUrl", content: info.licenseUrl});
   }
 
   if (resourceListing.info) { convertInfo(resourceListing.info); }
