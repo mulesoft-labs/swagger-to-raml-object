70163efa3504c411e1feb4936bac369dda84a2d3
Blake Embrey <hello@blakeembrey.com>
2014-12-05 16:16:26 -0800

    Allow the CLI to specify a directory

--- bin/swagger-to-raml-object.js
+++ bin/swagger-to-raml-object.js
@@ -1,19 +1,63 @@
 #!/usr/bin/env node
 
 var fs        = require('fs');
+var path      = require('path');
 var request   = require('request');
-var normalize = require('path').normalize;
 var converter = require('../');
-var args      = process.argv.slice(2);
+var filename  = process.argv[2];
 
-if (!args.length) {
+if (!filename) {
   console.error();
   console.error('Usage: swagger-to-raml-object api-docs');
   console.error();
   process.exit(1);
 }
 
-converter(args[0], function (filename, done) {
+if (!/^\w+\:\/\//.test(filename) && fs.statSync(filename).isDirectory()) {
+  converter.files(getFiles(filename), filereader, complete)
+} else {
+  converter(filename, filereader, complete);
+}
+
+/**
+ * Synchronous and recursively get all files in a directory.
+ *
+ * @param  {String} dirname
+ * @return {Object}
+ */
+function getFiles (dirname) {
+  var list    = [];
+  var visited = [];
+
+  (function readdir (dirname) {
+    var files = fs.readdirSync(dirname);
+
+    files.forEach(function (file) {
+      var filename = path.join(dirname, file);
+
+      if (fs.statSync(filename).isDirectory()) {
+        if (visited.indexOf(filename) > -1) {
+          throw new Error('Stuck in recursive loop');
+        }
+
+        visited.push(filename);
+        return readdir(filename);
+      }
+
+      list.push(filename);
+    });
+  })(dirname);
+
+  return list;
+}
+
+/**
+ * Read from the filesystem and remote URL.
+ *
+ * @param {String}   filename
+ * @param {Function} done
+ */
+function filereader (filename, done) {
   if (/https?:\/\//i.test(filename)) {
     return request(filename, {
       rejectUnauthorized: false
@@ -22,12 +66,20 @@ converter(args[0], function (filename, done) {
     });
   }
 
-  return fs.readFile(normalize(filename), 'utf8', done);
-}, function (err, ramlObject) {
+  return fs.readFile(filename, 'utf8', done);
+}
+
+/**
+ * Callback to run when Swagger has been generated.
+ *
+ * @param {Error}  err
+ * @param {Object} ramlObject
+ */
+function complete (err, ramlObject) {
   if (err) {
     console.error(err.stack);
     return process.exit(1);
   }
 
   console.log(JSON.stringify(ramlObject));
-});
+}
